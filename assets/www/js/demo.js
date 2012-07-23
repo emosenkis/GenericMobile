a802c7a26fa04ced5e98f1357d9039dadeeae0c2
Eitan Mosenkis <eitan@mosenkis.net>
2012-07-22 21:38:42 -0400

    Turned into MetaLister mobile app

--- assets/www/js/demo.js
+++ assets/www/js/demo.js
@@ -1,61 +1,29 @@
+var server='http://localhost:3000';
 
-$("#createtourpage").live("pagecreate", function(event) {
+$("#show").live("pageinit", function(event) {
 	var page = $("#createtourpage");
-	var watchGeoID = null;
-	var lastpos = null;
-	
-	onCurrentGeoSuccess = function(position) {
-
-		var s = 'Latitude: '    + position.coords.latitude          + '<br/>' +
-          'Longitude: '         + position.coords.longitude         + '<br/>' +
-          'Altitude: '          + position.coords.altitude          + '<br/>' +
-          'Accuracy: '          + position.coords.accuracy          + '<br/>' +
-          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '<br/>' +
-          'Heading: '           + position.coords.heading           + '<br/>' +
-          'Speed: '             + position.coords.speed             + '<br/>' +
-          'Timestamp: '         + new Date(position.timestamp)      + '<br/>';
-        lastpos = position;
-		  
-		$("#status",page).html(s);
-	};
-	
-	onGeoError = function() {
-		$("#status",page).html("An error with the geolocation. Sorry");
-	};
-	
-	$("#currentLink",page).live("click",function(e) {
-		$("#status",page).html("Getting location...<br/>");
-		navigator.geolocation.getCurrentPosition(onCurrentGeoSuccess, onGeoError);
-		e.preventDefault();
-	});
-
-	$("#startTrackingLink",page).live("click",function(e) {
-		$("#status",page).html("Tracking began...<br/>");
-		watchGeoID = navigator.geolocation.watchPosition(onCurrentGeoSuccess, onGeoError);
-		e.preventDefault();
-	});
-
-	$("#stopTrackingLink",page).live("click",function(e) {
-		$("#status",page).html("Tracking stopped...<br/>");
-		if (watchGeoID) {
-			navigator.geolocation.clearWatch(watchGeoID);
-			watchGeoID = null;
+	$.get(server+'/mobiledownload.json', {}, function(data, textStatus, jqXHR) {
+		var html='<ul>';
+		for (var i=0; i<data.length; i++) {
+			listing=data[i];
+			html+='<li><b>'+listing.title+'</b><p>'+listing.descr+'</p>';
+			if (listing.url) {
+				html+='URL: <a href="'+listing.url+'">'+listing.url+'</a>';
+			}
+			html+='</li>';
 		}
-		e.preventDefault();
-	});
+		html+='</ul>';
+		$('#listings').html(html);
+	}, 'jsonp');
+});
 
-
-	$("#getDataLink").live("click",function(e) {
-		$.post("http://129.64.237.108:3000/test.json", 
-		        {"lat":lastpos.coords.latitude, 
-		         "lon":lastpos.coords.longitude, 
-		         "tour":$("#tourname",page).val(),
-		         "title":$("#title",page).val(), 
-		         "descr":$("#descr",page).val()}, 
-		        function(res,code) {
-			         $("#status", page).html("Result from remote server was " + res[0].title);
-		});
-	});
-
-	
+$("#submit").live('click', function() {
+	$.post(server+'/mobileupload.json', $('#create').serialize(), function(data) {
+		if (data.error) {
+			alert(data.error);
+		} else {
+			alert('Listing created.');
+			$.mobile.changePage('index.html');
+		}
+	}, 'jsonp');
 });
